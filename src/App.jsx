4a73fc9b6a6d9abedd70be832304350b08eeeec6
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-03 16:48:57 -0700

    dark theme toggling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import './App.css';
 
+
+
 function App() {  
   const clicked= ()=>{
-    let text = document.getElementById("text")
-    let vanish = document.getElementById("vanish")
-    document.body.style.backgroundColor = "blue"
-    text.innerHTML = 'clicked';
-    vanish.style.visibility = "hidden";
+    document.body.classList.toggle('dark-mode');
+   
   }
-  const showButton= ()=>{
-    let vanish = document.getElementById("vanish")
-    vanish.style.visibility = "visible";
-  }
+  
   return (
-    <div className="App">
-      <h1 id='text'>new one</h1>
+    <div className="container">
+      <h1 id='text' className='text'>new one</h1>
       <button onClick={clicked}>click me</button>
-      <button id='vanish'>test</button>
-      <button onClick={showButton}>test</button>
     </div>
   );
 }
