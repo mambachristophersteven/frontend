66b9d25a93f1eddb2f5c6f62b10d64484355176a
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-03 16:39:07 -0700

    added visibility feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
 
-function App() {
+function App() {  
   const clicked= ()=>{
     let text = document.getElementById("text")
     let vanish = document.getElementById("vanish")
     document.body.style.backgroundColor = "blue"
     text.innerHTML = 'clicked';
-    vanish.style.display = "none";
+    vanish.style.visibility = "hidden";
+  }
+  const showButton= ()=>{
+    let vanish = document.getElementById("vanish")
+    vanish.style.visibility = "visible";
   }
   return (
     <div className="App">
       <h1 id='text'>new one</h1>
       <button onClick={clicked}>click me</button>
       <button id='vanish'>test</button>
+      <button onClick={showButton}>test</button>
     </div>
   );
 }
